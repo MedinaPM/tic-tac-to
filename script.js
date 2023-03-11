e8cbffdcc5f59d8baa38ceb60a9a51a6ea598868
Javier <javier.medina15@gmail.com>
2023-03-10 20:13:14 -0500

    Add a basic Game Controller module

--- script.js
+++ script.js
@@ -57,51 +57,54 @@ const gameBoard = (() => {
 })();
 
 // Module Pattern Game Controller
+const gameController = (() => {
+  const clickListener = (e) => {
+    switch (e.target.id) {
+      case "nav__button-single-player":
+        gameBoard.newBoard();
+        break;
+      case "nav__button-two-players":
+        gameBoard.getForm();
+        break;
+      case "button-new-game":
+        gameBoard.newGame();
+        break;
+      case "players-name-form__button-submit":
+        gameBoard.getPlayersNames();
+        break;
+      case "game-board__A1":
+        console.log("game-board__A1 button");
+        break;
+      case "game-board__B1":
+        console.log("game-board__B1 button");
+        break;
+      case "game-board__C1":
+        console.log("game-board__C1 button");
+        break;
+      case "game-board__A2":
+        console.log("game-board__A2 button");
+        break;
+      case "game-board__B2":
+        console.log("game-board__B2 button");
+        break;
+      case "game-board__C2":
+        console.log("game-board__C2 button");
+        break;
+      case "game-board__A3":
+        console.log("game-board__A3 button");
+        break;
+      case "game-board__B3":
+        console.log("game-board__B3 button");
+        break;
+      case "game-board__C3":
+        console.log("game-board__C3 button");
+        break;
+      default:
+    }
+  };
+  return {
+    clickListener,
+  };
+})();
 
-// click listener
-function clickListener(e) {
-  switch (e.target.id) {
-    case "nav__button-single-player":
-      gameBoard.newBoard();
-      break;
-    case "nav__button-two-players":
-      gameBoard.getForm();
-      break;
-    case "button-new-game":
-      gameBoard.newGame();
-      break;
-    case "players-name-form__button-submit":
-      gameBoard.getPlayersNames();
-      break;
-    case "game-board__A1":
-      console.log("game-board__A1 button");
-      break;
-    case "game-board__B1":
-      console.log("game-board__B1 button");
-      break;
-    case "game-board__C1":
-      console.log("game-board__C1 button");
-      break;
-    case "game-board__A2":
-      console.log("game-board__A2 button");
-      break;
-    case "game-board__B2":
-      console.log("game-board__B2 button");
-      break;
-    case "game-board__C2":
-      console.log("game-board__C2 button");
-      break;
-    case "game-board__A3":
-      console.log("game-board__A3 button");
-      break;
-    case "game-board__B3":
-      console.log("game-board__B3 button");
-      break;
-    case "game-board__C3":
-      console.log("game-board__C3 button");
-      break;
-    default:
-  }
-}
-
-document.addEventListener("click", clickListener);
+document.addEventListener("click", gameController.clickListener);
